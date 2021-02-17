946b2075d8c3826f61b99d8a3a0f653b8f440cc3
Emanuele1927 <35915808+Emanuele1927@users.noreply.github.com>
2021-02-17 16:52:59 +0100

    Sviluppato Automa

--- DistributoreCaffè/src/distributorecaffè/AutomaDistributore.java
+++ DistributoreCaffè/src/distributorecaffè/AutomaDistributore.java
@@ -5,20 +5,33 @@ import jdk.jfr.Event;
 public class AutomaDistributore implements State {
 
     private State stato;
+    float tot = 0.45f;
 
     public AutomaDistributore() {
+        stato = new Attesa();
     }
 
     @Override
     public void next(Event e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        stato.next(e);
     }
 
     private class Attesa implements State {
 
         @Override
         public void next(Event e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+            if (e instanceof Caffè) {
+                if (tot < 0.45f) {
+                    stato = new Attesa();
+                    System.out.println("Inserire almeno € 0.45");
+                } else if (tot > 0.45f) {
+                    stato = new Erogazione();
+                }
+
+            } else {
+                System.out.println("Errore");
+            }
         }
 
     }
